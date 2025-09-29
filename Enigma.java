5ca27b7d790d40127140dc63fdebb7fceb46c141
Webb_Richard <r.webb1@gwmail.gwu.edu>
2025-09-29 00:10:39 -0400

    my first commit

--- Enigma.java
+++ Enigma.java
@@ -8,6 +8,9 @@ public class Enigma{
 
 
     private Rotor rotors[];
+
+    //hardcode the alphabet in here as our fixed outer ring
+    private String s = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         
     public Enigma(int id1, int id2, int id3, String start){
 
@@ -21,12 +24,64 @@ public class Enigma{
 
     public String decrypt(String message){        
         //TODO
+        String result = ""; //start with an empty string variable called result where we will hold everything later
+
+        //loop through message
+        for (int i = 0; i < message.length(); i++) {
+            //find the character
+            char character = message.charAt(i);
+
+            //this time, we go backward instead of forwards (like in encryption)
+            //first, finding the position inside rotor 2 which is where we ended in encryption
+            int index = rotors[2].indexOf(character);
+            //map that back to the outer ring
+            character = rotors[1].charAt(index);
+
+            //now go through rotor 1 except in reverse
+            index = rotors[2].indexOf(character);
+            character = rotors[0].charAt(index); //map that back to the outer ring again
+
+            //append that to the result
+            result = result + character;
+            //then rotate after each character
+            rotate();
+
+        }
+        //return that result
+        return result;
+
+        
     }
 
 
     
     public String encrypt(String message){
         //TODO
+        //create a result that we add to later
+        String result = "";
+
+        //iterate through the message
+        for (int i = 0; i < message.length(); i++) {
+
+            //find the character
+            char character = message.charAt(i);
+
+            //find the position of that character in the outer ring
+            int index = rotors[0].indexOf(character);
+
+            //go through the 0th rotor
+            character = rotors[2].charAt(index);
+            //convert that back to an index in the second ring
+            index = rotors[1].indexOf(character);
+
+            //then go through that first rotor and keep rinsing and repeating this
+            character = rotors[2].charAt(index);
+            result = result + character;
+            //then rotate after each character is completely found 
+            rotate();
+        }
+        //return the full result
+        return result;
     }
 
     
--- Rotor.java
+++ Rotor.java
@@ -11,19 +11,53 @@ public class Rotor {
         while(!this.rotate());
             
     }
-    
+    //abc -- cab -- bca -- 
     public boolean rotate(){
         //TODO
+        char startingchar = rotorValues.charAt(rotorValues.length() - 1);
+        //find the last point in the array and add that to the front while also minusing from the end
+        rotorValues = startingchar + rotorValues.substring(0, rotorValues.length()-1);
+
+        if (rotorValues.charAt(0) == startChar) {
+            return true;
+        }else{
+            return false;
+        }
+
+
                
     }
     
 
     public int indexOf(char c){
         //TODO
+        //loop through the array 
+        for (int i = 0; i < rotorValues.length(); i++){
+            //if the character at i is equal to the index we put in (c)
+            if (rotorValues.charAt(i) == c){
+                //return that index
+                return (i);
+            }
+        }
+        return -1; //failed if it didnt find i since it would return later.
     }
 
     public char charAt(int idx){
         //TODO
+        //sanity check
+        if (idx < 0 || idx >= rotorValues.length()){
+            return '?';//character type to return 
+        }
+
+        //go through the array
+        for (int i = 0; i < rotorValues.length(); i++){
+            //find where i equals idx
+            if (i == idx){
+                //return the character at that position
+                return rotorValues.charAt(i); //I DONT KNOW HOW TO CREATE MY OWN CHARAT JAVA CAN DO IT FOR ME
+            }
+        }
+        return '?';//indicating failure
     }
 }
     
